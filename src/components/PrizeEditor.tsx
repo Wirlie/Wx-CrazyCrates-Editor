9f92275542bc56ec99e3454295ea41f075721e6a
Josue Acevedo <josue.211996@outlook.com>
2021-08-27 19:18:23 -0500

    Translate "general tab" of the prize editor.

--- src/components/PrizeEditor.tsx
+++ src/components/PrizeEditor.tsx
@@ -325,14 +325,14 @@ function PrizeEditor(props: Props) {
                     </div>
                     <div className="mb-4 row g-0">
                         <div className="col-6">
-                            <label htmlFor="input-display-lore" className="form-label font-weight-bold"><FontAwesomeIcon icon={faPen} /> Descripción del Premio:</label>
-                            <textarea onChange={(e) => handleLore(e.target.value)} id="input-display-lore" className="form-control" aria-describedby="displayLoreHelpBlock" value={displayLore.join("\n")} placeholder="Ingresa una descripción (opcional)..." />
+                            <label htmlFor="input-display-lore" className="form-label font-weight-bold"><FontAwesomeIcon icon={faPen} /> {t("edit_prize_lore")}</label>
+                            <textarea onChange={(e) => handleLore(e.target.value)} id="input-display-lore" className="form-control" aria-describedby="displayLoreHelpBlock" value={displayLore.join("\n")} placeholder={t("edit_prize_lore_placeholder")} />
                             <div id="displayLoreHelpBlock" className="form-text">
-                                Esta descripción se mostrará en el Menú de premios de esta caja para este premio, cada línea equivale a una línea en el mensaje flotante de Minecraft.
+                                {t("edit_prize_lore_description")}
                             </div>
                         </div>
                         <div className="col-5 offset-1">
-                            <div className="font-weight-bold"><FontAwesomeIcon icon={faSearch} /> Previsualización:</div>
+                            <div className="font-weight-bold"><FontAwesomeIcon icon={faSearch} /> {t("edit_prize_lore_preview")}</div>
                             <ItemTooltip>
                                 <ItemTooltipFormat title={displayName.length === 0 ? materialName : displayName}>
                                     { displayLore.length > 1 || (displayLore.length === 1 && displayLore[0].length !== 0)
@@ -351,37 +351,37 @@ function PrizeEditor(props: Props) {
                         </div>
                     </div>
                     <div className="mb-4">
-                        <label htmlFor="input-range" className="form-label font-weight-bold"><FontAwesomeIcon icon={faCog} /> Rango:</label>
+                        <label htmlFor="input-range" className="form-label font-weight-bold"><FontAwesomeIcon icon={faCog} /> {t("edit_prize_range")}</label>
                         <input onChange={(e) => handleRange(e.target.value)} value={maxRange} type="number" id="input-range" className="form-control" aria-describedby="rangeHelpBlock" min={1} />
                         <div id="rangeHelpBlock" className="form-text">
-                            Rango del cálculo del % de chance, se recomienda dejar en 100 a menos que conozcas como funciona este cálculo.
+                            {t("edit_prize_range_description")}
                         </div>
                     </div>
                     <div className="mb-4">
-                        <label htmlFor="input-chance" className="form-label font-weight-bold"><FontAwesomeIcon icon={faCog} /> Chance:</label>
+                        <label htmlFor="input-chance" className="form-label font-weight-bold"><FontAwesomeIcon icon={faCog} /> {t("edit_prize_chance")}</label>
                         <input onChange={(e) => handleChance(e.target.value)} value={chance} type="number" id="input-chance" className="form-control" aria-describedby="chanceHelpBlock" min={1} />
                         <div id="chanceHelpBlock" className="form-text">
-                            Define la chance de este objeto, ten en cuenta que el cálculo se hace en base a la chance total de todos los premios.
+                            {t("edit_prize_chance_description")}
                         </div>
                     </div>
                     <div className="mb-4">
-                        <label htmlFor="input-firework" className="form- font-weight-bold"><FontAwesomeIcon icon={faCog} /> Efecto de Cohete:</label>
+                        <label htmlFor="input-firework" className="form- font-weight-bold"><FontAwesomeIcon icon={faCog} /> {t("edit_prize_firework")}</label>
                         <select defaultValue={firework ? "true" : "false"} className="form-control" id="input-firework" aria-describedby="fireworkHelpBlock" onChange={(e) => handleFireworkSelectChange(e.target.value)}>
-                            <option value="true">SI</option>
-                            <option value="false">NO</option>
+                            <option value="true">{t("select_op_yes")}</option>
+                            <option value="false">{t("select_op_no")}</option>
                         </select>
                         <div id="fireworkHelpBlock" className="form-text">
-                            Habilitar este efecto hará que explote un Cohete en la ubicación de la caja cuando este premio le toque a un jugador.
+                            {t("edit_prize_firework_description")}
                         </div>
                     </div>
                     <div className="mb-4">
-                        <label htmlFor="input-glowing" className="form-label font-weight-bold"><FontAwesomeIcon icon={faCog} /> Efecto de Brillo:</label>
+                        <label htmlFor="input-glowing" className="form-label font-weight-bold"><FontAwesomeIcon icon={faCog} /> {t("edit_prize_glow_effect")}</label>
                         <select defaultValue={glowing ? "true" : "false"} className="form-control" id="input-glowing" aria-describedby="glowingHelpBlock" onChange={(e) => handleGlowingSelectChange(e.target.value)}>
-                            <option value="true">SI</option>
-                            <option value="false">NO</option>
+                            <option value="true">{t("select_op_yes")}</option>
+                            <option value="false">{t("select_op_no")}</option>
                         </select>
                         <div id="glowingHelpBlock" className="form-text">
-                            Habilitar este efecto hará que el ítem brille con el efecto de encantamiento (el mismo que obtienes cuando encantas un ítem desde la mesa de encantamientos).
+                            {t("edit_prize_glow_effect_description")}
                         </div>
                     </div>
                 </div>
